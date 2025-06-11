b82a55d6edb2fc28c9bc893b41d520701ff8f591
wmvj <wmvj@unifesspa.edu.br>
2025-06-11 14:09:56 -0300

    Add comentarios exemplos

--- JS/Vetores/arrayGeral.js
+++ JS/Vetores/arrayGeral.js
@@ -1,28 +1,28 @@
 console.log(typeof Array, typeof new Array, typeof [])
 
 const alunos = ['Alex', 'Samuel', 'Luna', 'Ruan']
-alunos.pop()
+alunos.pop() // remove o ultimo elemento do array
 console.log(alunos)
 
-alunos.push('Ruan')
+alunos.push('Ruan') // adiciona um elemento no final do array
 console.log(alunos)
 
-alunos.shift()
+alunos.shift() // remove o primeiro elemento do array
 console.log(alunos)
 
-alunos.unshift('Alex')
+alunos.unshift('Alex') // adiciona um elemento no inicio do array
 console.log(alunos)
 
-alunos.splice(2, 0, 'Pedro', 'Wesley')
+alunos.splice(2, 0, 'Pedro', 'Wesley') // add elementos do array no indice 2
 console.log(alunos)
 
-alunos.splice(0, 1)
+alunos.splice(0, 1) // remove um elemento do array no indice 0
 console.log(alunos)
 
-const alunos2 = alunos.slice(2)
+const alunos2 = alunos.slice(2) // cria um novo array a partir do indice 2 do array original
 console.log(alunos2)
 
-const alunos3 = alunos.slice(1,4)
+const alunos3 = alunos.slice(1,4) // cria um novo array a partir do indice 1 até o indice 4 do array original
 console.log(alunos3)
 
 
