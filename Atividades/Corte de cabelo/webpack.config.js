a11cce7eed119d55fdcdac6579ab5c60e43dc8a7
wmvj <wmvj@unifesspa.edu.br>
2025-06-17 12:29:11 -0300

    Add Projeto Corte de Cabelo

--- Atividades/Corte de cabelo/webpack.config.js
+++ Atividades/Corte de cabelo/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     target: "web",
@@ -21,6 +22,33 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname,'index.html'),
             favicon: path.resolve("src", "assets", "scissors.svg"),
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { 
+                    from: path.resolve(__dirname, 'src', 'assets'), 
+                    to: path.resolve(__dirname, 'dist', "src", 'assets') 
+                },
+            ],
         })
-    ]
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
 }
